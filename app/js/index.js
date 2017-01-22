52f46b876d07d9451cc20c00700907a1bb42e8ea
German Hernandez <muygrafico@gmail.com>
2017-01-22 00:57:11 -0500

    upgrade visual of unavailable products | add method to test add to cart button

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,9 @@
 
 (function () {
-  Vue.component('item',{
+  Vue.component('product',{
     template:
-      `<div class="column is-one-quarter">
-        <div class="product">
+      `<div class="column is-one-third">
+        <div class="product" v-bind:class="{ unavailable: !available }">
           <div class="image">
             <span class="icon best-seller" v-show="best_seller">
               <i class="fa fa-star"></i>
@@ -19,33 +19,39 @@
             <div class="price">$\{{ price }}</div>
           </div>
           <div class="button-container">
-            <button v-show="available" class="button is-primary">Add to cart</button>
-            <span v-show="!available" class="button is-disabled">Product not available</span>
+            <button v-show="available" v-on:click="addToCart(id)" class="button is-primary">Add to cart</button>
+            <span v-show="!available" class="button is-disabled">Not available</span>
           </div>
 
         </div>
       </div>`,
-  props: ['img', 'name', 'price', 'description', 'best_seller', 'available'],
+  props: ['img', 'name', 'price', 'description', 'best_seller', 'available', 'id'],
   computed: {
     pic: function () {
       return this.img + '/' + Math.floor((Math.random() * 10) + 1);
     }
+  },
+  methods: {
+    addToCart: function (product) {
+      console.log(product)
+    }
    }
+
   })
 
   Vue.component('store',{
     template:
       `<div class="columns is-multiline">
-        <item
+        <product
         :img="product.img"
         :name="product.name"
         :price="product.price"
         :description="product.description"
         :best_seller="product.best_seller"
         :available="product.available"
-        v-for="(product, index) in products"></item>
-      </div>`
-      ,
+        :id="product.id"
+        v-for="(product, index) in products"></product>
+      </div>`,
     data() {
       return {
         categories: [],
