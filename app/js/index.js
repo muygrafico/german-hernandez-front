f0642bd94e52778e4b5cacebc095f72ff1070c6f
German Hernandez <muygrafico@gmail.com>
2017-01-23 06:10:17 -0500

    filters sideMenu WIP 5

--- app/js/index.js
+++ app/js/index.js
@@ -2,64 +2,59 @@
 
 (function () {
 
-Vue.component('inputFilter',{
-  template:
-    `<div class="input-filter">
-      <p class="control">
-        <input
-        class="input"
-        type="text"
-        placeholder="Search"
-        v-on:keyup="filter({text: filterText})"
-        v-model="filterText">
-        <a v-show="this.filterText !== ''" @click="resetFilterText" class="delete is-small"></a>
-      </p>
-    </div>`,
-    data() {
-      return {
-        filterText: ''
-      }
-    },
-    methods: {
-      resetFilterText: function() {
-        this.filterText = ''
-        this.filter({text: this.filterText})
-      }
-    },
-    props: ['filter']
-})
+  Vue.component('inputFilter',{
+    template:
+      `<div class="input-filter">
+        <p class="control">
+          <input
+          class="input"
+          type="text"
+          placeholder="Search"
+          v-on:keyup="updateFilterText(filterText)"
+          v-model="filterText">
+          <a v-show="this.filterText !== ''" @click="resetFilterText" class="delete is-small"></a>
+        </p>
+      </div>`,
+      data() {
+        return {
+          filterText: ''
+        }
+      },
+      methods: {
+        resetFilterText: function() {
+          this.filterText = ''
+          this.updateFilterText('')
+        }
+      },
+      props: ['updateFilterText', 'currentActiveFilterId']
+    })
 
-Vue.component('filtersMenu',{
-  template:
-    `<aside class="menu filter-menu">
-      <p class="menu-label">
-        Filters
-      </p>
-
-      <inputFilter :filter="this.filter"></inputFilter>
-
-      <ul class="menu-list">
-        <li>
-          <h5>Filters:</h5>
-          <ul>
-            <li v-for="filter in categories">
-              <a v-bind:class="{ 'is-active': filter.id === currentActiveFilter() }" @click="toggleActiveFilter({name: filter.name, id: filter.id })">{{ filter.name }}</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <h5>Order by:</h5>
-          <ul>
-            <li><a>Name</a></li>
-            <li><a>Higher price</a></li>
-            <li><a>Lower price</a></li>
-          </ul>
-        </li>
-      </ul>
-
-    </aside>`,
-    props: ['categories', 'filter', 'toggleActiveFilter','currentActiveFilter']
-})
+    Vue.component('filtersMenu',{
+      template:
+      `<aside class="menu filter-menu">
+        <p class="menu-label"> Filters</p>
+        <inputFilter :updateFilterText="this.updateFilterText" :currentActiveFilterId="currentActiveFilterId"></inputFilter>
+        <ul class="menu-list">
+          <li>
+            <h5>Filters:</h5>
+            <ul>
+              <li v-for="filter in categories">
+                <a v-bind:class="{ 'is-active': filter.id === currentActiveFilterId() }" @click="toggleActiveFilter({name: filter.name, id: filter.id })">{{ filter.name }}</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h5>Order by:</h5>
+            <ul>
+              <li><a>Name</a></li>
+              <li><a>Higher price</a></li>
+              <li><a>Lower price</a></li>
+            </ul>
+          </li>
+        </ul>
+      </aside>`,
+      props: ['categories', 'updateFilterText', 'toggleActiveFilter','currentActiveFilterId']
+    })
 
   Vue.component('product',{
     template:
@@ -90,24 +85,20 @@ Vue.component('filtersMenu',{
           </div>
         </div>
       </div>`,
-  props: ['img', 'name', 'price', 'description', 'best_seller', 'available', 'id', 'productCategories', 'storeCategories'],
-  computed: {
-    pic: function () {
-      return this.img + '/' + Math.floor((Math.random() * 10) + 1)
-    }
-  },
-  methods: {
-    addToCart: function (product) {
-      console.log(product)
-    },
-    findCategoryById: function (id) {
-      return this.storeCategories.filter(function(v) {
-        return v.id === id
-      })[0].name
-    }
-   }
-
-  })
+      props: ['img', 'name', 'price', 'description', 'best_seller', 'available', 'id', 'productCategories', 'storeCategories'],
+      computed: {
+        pic: function () {
+          return this.img + '/' + Math.floor((Math.random() * 10) + 1)
+        }
+      },
+      methods: {
+        findCategoryById: function (id) {
+          return this.storeCategories.filter(function(v) {
+            return v.id === id
+          })[0].name
+        }
+      }
+    })
 
   Vue.component('store',{
     template:
@@ -115,10 +106,10 @@ Vue.component('filtersMenu',{
         <div class="columns">
           <div class="column is-2">
             <filtersMenu
-            :filter="this.filter"
+            :updateFilterText="this.updateFilterText"
             :toggleActiveFilter="toggleActiveFilter"
             :categories="categories"
-            :currentActiveFilter="currentActiveFilter"
+            :currentActiveFilterId="currentActiveFilterId"
             ><filtersMenu>
           </div>
           <div class="column">
@@ -146,36 +137,13 @@ Vue.component('filtersMenu',{
         products: [],
         filteredProducts: [],
         cart: [],
-        activeFilter: {name: null, id: 0},
+        activeFilter: {name: '', id: 0},
         filterText: ''
-
       }
     }, mounted(){
       axios.get('json/data.json').then(
         (response) => {
           this.categories = response.data.categories
-          // this.categories.push(
-          //   {
-          //     "id": 5,
-          //     "name": "available"
-          //   },
-          //   {
-          //     "id": 6,
-          //     "name": "unavailable"
-          //   },
-          //   {
-          //     "id": 7,
-          //     "name": "best seller"
-          //   },
-          //   {
-          //     "id": 8,
-          //     "name": "higher than $30,000"
-          //   },
-          //   {
-          //     "id": 9,
-          //     "name": "lower than $10,000"
-          //   }
-          // )
           this.products = response.data.products
           this.filteredProducts = this.products
         }
@@ -184,52 +152,39 @@ Vue.component('filtersMenu',{
     methods: {
       addToCart: function (productId) {
         this.cart.push(productId)
-        // console.log(this.cart)
+        console.log(this.cart)
       },
-      // filter: function(options) {
-      //   this.filteredProducts = this.products.filter(el => {
-      //     return el.name.toLowerCase().includes(options.text.toLowerCase())
-      //   })
-      // },
       filter: function(options) {
-      // if (options.text !== '' ){ this.filterText = options.text }
-      console.log(this.filterText)
-       var id = this.activeFilter.id
+        console.log(this.activeFilter.id)
        this.filteredProducts = this.products.filter(el => {
-
-        //  var result =
-        //    el.name.toLowerCase().includes(options.text.toLowerCase())
-         //
-        //  return result
-
-         function checkCategory(categoryId) {
-           console.log(id)
-           if (id !== null) {
-             return categoryId === id && el.name.toLowerCase().includes(options.text.toLowerCase())
+         return el.categories.find((categoryId)=> {
+           if (this.activeFilter.id === 0) {
+             return el.name.toLowerCase().includes(this.filterText.toLowerCase())
            } else {
-             return el.name.toLowerCase().includes(options.text.toLowerCase())
+             return el.name.toLowerCase().includes(this.filterText.toLowerCase()) &&  categoryId === this.activeFilter.id
            }
-
-         }
-
-         return el.categories.find(checkCategory)
-         console.log(el.categories)
-
-
+         })
        });
      },
      toggleActiveFilter: function(filterObj) {
-        filterObj.id === this.activeFilter.id ? this.activeFilter = {name: null, id: null} : this.activeFilter =  filterObj
-        console.log(this.activeFilter.name)
-        this.filter({text: this.filterText, activeFilterId: this.activeFilter.id })
+        filterObj.id === this.activeFilter.id ? this.activeFilter = {name: '', id: 0} : this.activeFilter =  filterObj
       },
-      currentActiveFilter: function() {
-         console.log('this.activeFilter')
-         return this.activeFilter.id
-       },
-
-    }
-
+      currentActiveFilterId: function() {
+        return this.activeFilter.id
+      },
+      updateFilterText: function(filterText) {
+        this.filterText = filterText
+        console.log(this.filterText)
+      }
+    },
+    watch: {
+     filterText: function () {
+      this.filter()
+     },
+      activeFilter: function () {
+      this.filter()
+     }
+   }
   })
 
   new Vue({el: '#storeApp'})
