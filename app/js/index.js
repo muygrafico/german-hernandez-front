d339b8f7d6f1ef2178120793da16f52ddfa28f2c
German Hernandez <muygrafico@gmail.com>
2017-01-22 22:08:36 -0500

    filters sideMenu WIP

--- app/js/index.js
+++ app/js/index.js
@@ -22,6 +22,58 @@ Vue.component('inputFilter',{
     props: ['filter']
 })
 
+Vue.component('filtersMenu',{
+  template:
+    `<aside class="menu filter-menu">
+      <p class="menu-label">
+        Filters
+      </p>
+      <inputFilter :filter="this.filter"></inputFilter>
+
+      <ul class="menu-list">
+        <li>
+          <h5>Categories</h5>
+          <ul>
+            <li v-for="category in categories">
+              <a>{{ category.name }}</a>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <h5>Other filters</h5>
+          <ul>
+            <li><a>Available</a></li>
+            <li><a>Unvailable</a></li>
+            <li><a>Best seller</a></li>
+          </ul>
+        </li>
+
+        <li>
+          <h5>Price</h5>
+          <ul>
+            <li><a>Higher than $30,000</a></li>
+            <li><a>Lower than $10,000</a></li>
+          </ul>
+        </li>
+        <li>
+          <h5>Order by:</h5>
+          <ul>
+            <li><a>Name</a></li>
+            <li><a>Higher price</a></li>
+            <li><a>Lower price</a></li>
+          </ul>
+        </li>
+      </ul>
+    </aside>`,
+    data() {
+      return {
+        categories: ''
+      }
+    },
+    props: ['categories']
+})
+
 
   Vue.component('product',{
     template:
@@ -67,20 +119,26 @@ Vue.component('inputFilter',{
   Vue.component('store',{
     template:
       `<div class="store">
-        <inputFilter :filter="this.filter"></inputFilter>
-        <div class="columns is-multiline">
-          <product
-          :img="product.img"
-          :name="product.name"
-          :price="product.price"
-          :description="product.description"
-          :best_seller="product.best_seller"
-          :available="product.available"
-          :id="product.id"
+        <div class="columns">
+          <div class="column is-2">
+            <filtersMenu :categories="categories"> <filtersMenu>
+          </div>
+          <div class="column">
+            <div class="columns is-multiline">
+              <product
+              :img="product.img"
+              :name="product.name"
+              :price="product.price"
+              :description="product.description"
+              :best_seller="product.best_seller"
+              :available="product.available"
+              :id="product.id"
 
-          v-for="product in filteredProducts">
-            <button class="button is-primary" @click="addToCart(product.id)">Add to cart</button>
-          </product>
+              v-for="product in filteredProducts">
+                <button class="button is-primary" @click="addToCart(product.id)">Add to cart</button>
+              </product>
+            </div>
+          </div>
         </div>
       </div>`,
     data() {
